f9553ff933e837da99fdd6e2a989783c873f9f42
Rodrigo <31673020+DoxCode@users.noreply.github.com>
2025-09-20 23:31:10 +0200

    Update ClearFailedMagnetHandler.cs

--- src/NzbDrone.Core/Download/ClearFailedMagnetHandler.cs
+++ src/NzbDrone.Core/Download/ClearFailedMagnetHandler.cs
@@ -21,22 +21,29 @@ namespace NzbDrone.Core.Download
 
             var trackedDownload = message.TrackedDownload;
 
-            if (trackedDownload?.RemoteMovie?.Movie == null)
+            var release = trackedDownload.RemoteMovie.Release;
+            _logger.Debug("ClearFailedMagnetHandler: Release no disponible, release.Guid: {0}, release.Title: {1}, release.InfoUrl: {2}, release.IndexerId: {3}", release?.Guid, release?.Title, release?.InfoUrl, release?.IndexerId);
+
+            var downloadItem = trackedDownload?.DownloadItem;
+            _logger.Debug("ClearFailedMagnetHandler: DownloadItem no disponible, downloadItem.Title: {0}, downloadItem.DownloadId: {1}, downloadItem.DownloadClientInfo.Id: {2}", downloadItem?.Title, downloadItem?.DownloadId, downloadItem?.DownloadClientInfo?.Id);
+
+            var parsedMovie = trackedDownload.RemoteMovie.ParsedMovieInfo;
+            _logger.Debug("ClearFailedMagnetHandler: ParsedMovie no disponible, parsedMovie.ImdbId: {0}, parsedMovie.OriginalTitle: {1}, parsedMovie.PrimaryMovieTitle: {2}, parsedMovie.ReleaseTitle: {3}", parsedMovie.ImdbId, parsedMovie.OriginalTitle, parsedMovie.PrimaryMovieTitle, parsedMovie.ReleaseTitle);
+
+            if (trackedDownload?.RemoteMovie?.Movie != null)
             {
-                _logger.Debug("ClearFailedMagnetHandler: TrackedDownload o Movie no disponible");
-                return;
+                var movie = trackedDownload.RemoteMovie.Movie;
+                _logger.Debug("ClearFailedMagnetHandler: MOVIE: TrackedDownload o Movie no disponible");
+
+                if (!string.IsNullOrWhiteSpace(movie.ExternalMagnet))
+                {
+                    movie.ExternalMagnet = "";
+                    _logger.Debug("ClearFailedMagnetHandler: No hay ExternalMagnet para película '{0}'", movie.Title);
+                    return;
+                }
+
+                _logger.Debug("__ENDED__");
             }
-
-            var movie = trackedDownload.RemoteMovie.Movie;
-
-            if (string.IsNullOrWhiteSpace(movie.ExternalMagnet))
-            {
-                _logger.Debug("ClearFailedMagnetHandler: No hay ExternalMagnet para película '{0}'", movie.Title);
-                return;
-            }
-
-            _logger.Info("ClearFailedMagnetHandler: Limpiando ExternalMagnet para película '{0}' (ID: {1}) debido a fallo de descarga", movie.Title, movie.Id);
-            movie.ExternalMagnet = "";
         }
     }
 }
